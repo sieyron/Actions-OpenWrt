68d3abfdd25d16a1d053a79353e6b9da3291e05d
sieyron <60006921+sieyron@users.noreply.github.com>
2025-02-01 16:53:21 +0000

    Add files

---
+++ diy-part2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# https://github.com/sieyron/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2025 sieyron
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/sieyron-Router/g' package/base-files/files/bin/config_generate
